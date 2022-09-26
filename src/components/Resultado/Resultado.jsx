bac2253200f83c9ed487ca47d25c83eb01118fd1
Luiz Henrique <58668840+luiz-tm@users.noreply.github.com>
2022-09-26 18:08:23 -0300

    Corrigido pequeno bug de definição

--- src/components/Resultado/Resultado.jsx
+++ src/components/Resultado/Resultado.jsx
@@ -1,9 +1,10 @@
 export default function Resultado(props) {
     const nome = localStorage.getItem('nome')
     const diaria = localStorage.getItem('diaria')
+
     return (
         <div className="mt-4">
-            <h2>O funcionário {nome} possui diária de R$ {diaria.toLocaleString()},00.</h2>
+            <h2>O funcionário {nome ? nome : 'Não definido'} possui diária de R$ {diaria ? diaria : 0},00.</h2>
         </div>
     )
 }
